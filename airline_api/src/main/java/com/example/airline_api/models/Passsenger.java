21c496eeb413d17d6b38ae02cd5828f16f4bf141
bendesousa <bdsousa069@gmail.com>
2022-09-09 10:50:38 +0100

    created passenger model

---
+++ airline_api/src/main/java/com/example/airline_api/models/Passsenger.java
@@ -0,0 +1,49 @@
+package com.example.airline_api.models;
+
+import javax.persistence.*;
+
+@Entity(name = 'passengers')
+public class Passsenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    public Passenger(String name, int phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Passsenger() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
